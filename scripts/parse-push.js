ba3d59db63e1fee5ef197b3de0d96d0c55d8d37b
TeoYM <S10262861@connect.np.edu.sg>
2024-11-12 11:04:22 +0800

    changes to parse-push.js

--- scripts/parse-push.js
+++ scripts/parse-push.js
@@ -7,7 +7,6 @@ const constants = {
   MONGO_URI: "mongodb+srv://teoyuanmao20:Password1234@cluster1.kv3es.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1",
 };
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(constants.MONGO_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,21 +17,16 @@ const client = new MongoClient(constants.MONGO_URI, {
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    // Define collection and database
     const db = client.db();
     const collection = db.collection('test_results');
 
-    // Get the current count of documents in the collection
     const count = await collection.countDocuments();
     console.log(`Current document count: ${count}`);
 
-    // Process test results
     const data = fs.readFileSync(constants.FILE_PATH);
     const obj = JSON.parse(data);
     const testResults = obj.testResults[0].testResults;
@@ -59,14 +53,11 @@ async function run() {
       };
     });
 
-    // Insert test results into MongoDB
     const result = await collection.insertMany(documents);
     console.log(`Inserted ${result.insertedCount} documents into the collection`);
 
-    // Summary statistics
     console.log(`Total tests: ${numTotalTests}\nTotal failed: ${numFailedTests}\nTotal passed: ${numPassedTests}\nTotal pending: ${numPendingTests}\nTotal todo: ${numTodoTests}\nTotal duration: ${totalDuration}s`);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
