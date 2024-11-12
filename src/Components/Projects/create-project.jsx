d2c1eebea6fbc792f1f74c4fda5f0cddda13cf5a
JunHao-NP <S10262731@connect.np.edu.sg>
2024-11-12 15:52:58 +0800

    Edited Popup box for create new project

--- src/Components/Projects/create-project.jsx
+++ src/Components/Projects/create-project.jsx
@@ -17,35 +17,43 @@ function CreateProject({ projCount, onClose }) {
 
   return (
     <div>
-        <div className="bgContain" onClick={onClose}>
-        </div>
+        <div className="bgContain" onClick={onClose}></div>
         <form className="popupBox">
-        <h1>New Project</h1>
-        <button type="button" onClick={onClose} className="closeButton closeTag">X</button>
-        <p className='closeTag2'>Click anywhere on the background to exit</p>
-        <div className="labelInput">
-          <label className="labelField">Project Name: </label>
-          <input type="text" placeholder={'New Project #' + projCount}></input>
-        </div>
-        <div className="labelInput">
-          <label className="labelField">Website: </label>
-          <input type="text"></input>
-        </div>
-        <div>
-          <h2>Visibility</h2>
-          <div className="labelInput" id="visBox">
-            <div className='radioBox'>
-              <input type="radio" className="visibility" id="public" name="visib" />
-              <label htmlFor="public">Public</label>
+            <h1>Create New Project</h1>
+            <div className="labelInput">
+                <label className="labelField">Project Name:</label>
+                <input type="text" placeholder={'New Project #' + projCount} className="inputField" />
             </div>
-            <div className="radioBox">
-              <input type="radio" className="visibility" id="private" name="visib" />
-              <label htmlFor="private">Private</label>
+
+            <div className="labelInput">
+                <label className="labelField">Website:</label>
+                <input type="text" className="inputField" />
             </div>
-          </div>
-        </div>
-        <button className="returnBtn2" onClick={() => console.log("CreateProj")}>Create</button>
-      </form>
+
+            <div className="labelInput">
+                <label className="labelField">Visibility:</label>
+                <div className="visibilityOptions">
+                    <label className="radioOption">
+                        <input type="radio" name="visibility" value="Public" />
+                        <span>Public</span>
+                    </label>
+                    <label className="radioOption">
+                        <input type="radio" name="visibility" value="Private" />
+                        <span>Private</span>
+                    </label>
+                </div>
+            </div>
+
+            <div className="fileUploadSection">
+                <label>Upload Test File:</label>
+                <div className="fileDropZone">Drag or drop to upload file</div>
+            </div>
+
+            <div className="buttonGroup">
+                <button type="button" className="cancelButton" onClick={onClose}>Cancel</button>
+                <button type="submit" className="createButton">Create Project</button>
+            </div>
+        </form>
     </div>
   );
 }
