0ed9707b8326b25239ccf9d462e50f2145824c6e
JunHao-NP <S10262731@connect.np.edu.sg>
2025-01-29 03:46:44 +0800

    Logout Function to log user out of account

--- src/Components/Login/profile.jsx
+++ src/Components/Login/profile.jsx
@@ -1,46 +1,69 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react';
+import { useNavigate } from "react-router-dom"; // Import useNavigate for redirection
 import logo from "../../assets/Logo-ocbc.png";
-import "./profile.css"
+import "./profile.css";
 
+function Profile() {
+    const navigate = useNavigate(); // Initialize navigate function
+    const [showLogoutModal, setShowLogoutModal] = useState(false); // State to manage the modal visibility
 
-function Profile(){
-  return (
-    <Fragment>
-        <form id="updateForm">
-            <div className="flexBox">
-                <h2 id="signInDescription">You are updating your user profile</h2>
-                <section id="iconNameDesc">
-                    <img src={logo} id="displayIcon" />
-                    <div id="displayName">User</div>
-                    <div id="displayDescription">Generic Description here.</div>
-                </section>
-                <h3 id="userId">Your user id: </h3>
-                <div id="logout" className="finalPress">Logout</div>
-            </div>
-            <div className="flexBox">
-                <label id="createUser" className="inputLabel padItem">Username</label>
-                <input type="text" name="username" id="username" />
-                <label id="changeIcon" className="inputLabel padItem">Icon URL</label>
-                <input type="text" name="iconUrl" id="iconUrl" />
-                <label id="changeDesc" className="inputLabel padItem">Description</label>
-                <input type="text" name="description" id="description" />
-                <label className="inputLabel padItem">Backup Email</label>
-                <input type="email" name="email" id="email" />
-                <label className="inputLabel padItem">Enter Old Password</label>
-                <input type="password" name="password" id="oldPassword" />
-                <label className="inputLabel padItem">Enter New Password</label>
-                <input type="password" name="password" id="newPassword" />
-                <label id="confirmLabel" className="inputLabel padItem">Confirm New Password</label>
-                <input type="password" name="confirmPassword" id="confirmPassword" />
-                <div className="buttons">   
-                    <button id="changeIcon">Change Icon</button>
-                    <button id="signNext" className="finalPress">Update Information</button>
-                    <div id="deleteUser" className="finalPress">Delete User</div>
+    const handleLogout = () => {
+        localStorage.removeItem('authToken'); // Remove the auth token from localStorage
+        setShowLogoutModal(true); // Show the logout modal
+        setTimeout(() => {
+            navigate("/"); // Redirect to the login page after a short delay
+        }, 2000); // Delay to allow the modal to show
+    };
+
+    return (
+        <Fragment>
+            <form id="updateForm">
+                <div className="flexBox">
+                    <h2 id="signInDescription">You are updating your user profile</h2>
+                    <section id="iconNameDesc">
+                        <img src={logo} id="displayIcon" />
+                        <div id="displayName">User</div>
+                        <div id="displayDescription">Generic Description here.</div>
+                    </section>
+                    <h3 id="userId">Your user id: </h3>
+                    {/* Add logout functionality */}
+                    <div id="logout" className="finalPress" onClick={handleLogout}>
+                        Logout
+                    </div>
                 </div>
-            </div>
-        </form>
-    </Fragment>
-  )
+                <div className="flexBox">
+                    <label id="createUser" className="inputLabel padItem">Username</label>
+                    <input type="text" name="username" id="username" />
+                    <label id="changeIcon" className="inputLabel padItem">Icon URL</label>
+                    <input type="text" name="iconUrl" id="iconUrl" />
+                    <label id="changeDesc" className="inputLabel padItem">Description</label>
+                    <input type="text" name="description" id="description" />
+                    <label className="inputLabel padItem">Backup Email</label>
+                    <input type="email" name="email" id="email" />
+                    <label className="inputLabel padItem">Enter Old Password</label>
+                    <input type="password" name="password" id="oldPassword" />
+                    <label className="inputLabel padItem">Enter New Password</label>
+                    <input type="password" name="password" id="newPassword" />
+                    <label id="confirmLabel" className="inputLabel padItem">Confirm New Password</label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" />
+                    <div className="buttons">
+                        <button id="changeIcon">Change Icon</button>
+                        <button id="signNext" className="finalPress">Update Information</button>
+                        <div id="deleteUser" className="finalPress">Delete User</div>
+                    </div>
+                </div>
+            </form>
+
+            {/* Logout Modal */}
+            {showLogoutModal && (
+                <div className="logout-modal">
+                    <div className="modal-content">
+                        <p>You have successfully logged out!</p>
+                    </div>
+                </div>
+            )}
+        </Fragment>
+    );
 }
 
 export default Profile;
